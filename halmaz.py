48c51242b69ca2f4b29c9b94d8cad84e4813711e
Persi Adam <persi.adam.2021e@ms.gdszeged.hu>
2022-11-14 09:33:11 +0100

    első feladat

---
+++ halmaz.py
@@ -0,0 +1,66 @@
+
+'''
+Halmaz (set):
+- rendezetlen (elemeknek nincs indexe)
+- egy elem csak egyszer szerepelhet
+- többféle típust tárolhat egyszerre is
+- a halmaz eleme maga nem változtatható meg
+'''
+
+# halmaz létrehozása
+reggeli = {'tea', 'vaj', 'piritós'}
+
+# üres halmaz létrehozása
+# ebed = {}  <- SZÓTÁRT hoz létre!!!
+ebed = set()
+# bejárható gyűjteményből, pl. listából
+ebed = set(['halászlé', 'kenyér', True])
+print(ebed)
+wr=open('halmaz.txt','w')
+wr.write()
+wr.close()
+
+
+# # egy elem hozzáadása
+# reggeli.add('lekvár')
+# # egy nem meghatározott elem eltávolítása
+# reggeli.pop()
+
+# # egy bizonyos elem eltávolítása
+# # ha nincs ilyen elem, akkor hibát eredményez
+# reggeli.remove('sajt')
+# # egy bizonyos elem eltávolítása
+# # ha nincs ilyen elem, nem eredményez hibát
+# reggeli.discard('sajt')
+
+
+# reggeli = {'víz', 'tea', 'vaj', 'pirítós'}
+# ebed = {'víz', 'halászlé', 'kenyér'}
+# # metszet
+# print(reggeli & ebed)
+# # unio
+# print(reggeli | ebed)
+# # különbség
+# print(reggeli - ebed)
+# # csak az egyikben, vagy csak a másikban
+# print(reggeli ^ ebed)
+
+
+
+# üres szótár létrehozása
+raktar = {}
+print(raktar)
+# szótár létrehozása adatokkal 
+diak = {
+      'vezeteknev': 'Kiss',
+      'keresztnev': 'Péter',
+      'eletkor': 17,
+      'menza': True
+}
+
+
+wr=open('szotar.txt','w')
+wr.write('vezeteknev: Kiss\n')
+wr.write('keresztnev: Péter\n')
+wr.write('eletkor: 17\n')
+wr.write('menza: True\n')
