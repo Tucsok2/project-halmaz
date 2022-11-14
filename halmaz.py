97931878b8f125e834477d614f886d6ef8e6ea5d
Persi Adam <persi.adam.2021e@ms.gdszeged.hu>
2022-11-14 10:30:42 +0100

    hazi

--- halmaz.py
+++ halmaz.py
@@ -1,11 +1,4 @@
 
-'''
-Halmaz (set):
-- rendezetlen (elemeknek nincs indexe)
-- egy elem csak egyszer szerepelhet
-- többféle típust tárolhat egyszerre is
-- a halmaz eleme maga nem változtatható meg
-'''
 
 # halmaz létrehozása
 reggeli = {'tea', 'vaj', 'piritós'}
@@ -21,17 +14,17 @@ wr.write()
 wr.close()
 
 
-# # egy elem hozzáadása
-# reggeli.add('lekvár')
-# # egy nem meghatározott elem eltávolítása
-# reggeli.pop()
+# egy elem hozzáadása
+reggeli.add('lekvár')
+# egy nem meghatározott elem eltávolítása
+reggeli.pop()
 
-# # egy bizonyos elem eltávolítása
-# # ha nincs ilyen elem, akkor hibát eredményez
-# reggeli.remove('sajt')
-# # egy bizonyos elem eltávolítása
-# # ha nincs ilyen elem, nem eredményez hibát
-# reggeli.discard('sajt')
+# egy bizonyos elem eltávolítása
+# ha nincs ilyen elem, akkor hibát eredményez
+reggeli.remove('sajt')
+# egy bizonyos elem eltávolítása
+# ha nincs ilyen elem, nem eredményez hibát
+reggeli.discard('sajt')
 
 
 # reggeli = {'víz', 'tea', 'vaj', 'pirítós'}
